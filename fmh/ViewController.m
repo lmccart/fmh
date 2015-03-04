cb48c423bf81832332bed03bb8eafeb6bdb18e49
Lauren McCarthy <laurmccarthy@gmail.com>
2015-03-04 17:50:16 -0500

    design tweaks

--- fmh/ViewController.m
+++ fmh/ViewController.m
@@ -12,6 +12,7 @@
 
 @property (retain, nonatomic) IBOutlet UIImageView *monitorStatusIcon;
 @property (retain, nonatomic) IBOutlet UILabel *hrLabel;
+@property (retain, nonatomic) IBOutlet UILabel *hrBattery;
 
 @end
 
@@ -20,6 +21,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    // Create the colors
+    UIColor *topColor = [UIColor colorWithRed:255.0/255.0 green:115.0/255.0 blue:214.0/255.0 alpha:1.0];
+    UIColor *bottomColor = [UIColor colorWithRed:251.0/255.0 green:222.0/255.0 blue:255.0/255.0 alpha:1.0];
+    
+    // Create the gradient
+    CAGradientLayer *theViewGradient = [CAGradientLayer layer];
+    theViewGradient.colors = [NSArray arrayWithObjects: (id)topColor.CGColor, (id)bottomColor.CGColor, nil];
+    theViewGradient.frame = self.view.bounds;
+    
+    //Add gradient to view
+    [self.view.layer insertSublayer:theViewGradient atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,7 +46,7 @@
 }
 
 - (void)updateMonitorBatteryLevel:(float)level {
-    [self.monitorStatusIcon setAlpha:level];
+    [self.hrBattery setText:[NSString stringWithFormat:@"%d%%", (int)(100*level)]];
 }
 
 - (void)updateHeartRate:(int)hr {
